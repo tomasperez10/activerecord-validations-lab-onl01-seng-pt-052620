35ee5426aa4dd1d34deef219d28fb12448a50754
Tomas Perez <tomasitopg6@gmail.com>
2020-10-03 16:51:02 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
-
+class ClickbaitValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /(Won't Believe|Secret|Top \d|Guess)/
+      record.errors[attribute] << ("Not a clickbait title")
+    end
+  end
+end
 
 
 
